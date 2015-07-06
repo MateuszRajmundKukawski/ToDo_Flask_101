b43465e0713493451f6711c33361fcd71d538132
MateuszRajmundKukawski <mrkukawski@gmail.com>
2015-07-06 20:21:20 +0200

    sqlite3 and flaks

--- todo/todo.py
+++ todo/todo.py
@@ -3,7 +3,11 @@
 
 from flask import Flask
 from flask import g
+from flask import render_template
+from flask import flash, redirect, url_for, request
 
+from datetime import datetime
+import sqlite3
 import os
 
 app = Flask(__name__)
@@ -36,9 +40,30 @@ def close_db(error):
 
 
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    return 'Pierwszy commit na poważnie'
+
+    error = None
+
+    if request.method == 'POST':
+        if len(request.form['zadanie']) > 0:
+            zadanie = request.form['zadanie']
+            zrobione = 0
+            data_pub = datetime.now()
+            db=get_db()
+            db.execute('insert into zadania (zadanie, zrobione, data_pub) values (?, ?, ?);',
+                        [zadanie, zrobione, data_pub])
+            db.commit()
+            return redirect(url_for('index'))
+        error = u'Nie moża dodać pustego zadania'
+
+
+    db = get_db()
+    kursor  = db.execute('select * from zadania order by data_pub desc;')
+    zadania = kursor.fetchall()
+    return render_template('zadania_lista.html', zadania=zadania, error=error)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
